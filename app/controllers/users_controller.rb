921c2dafda0efe18fb135993bd5d1ce81f8f822c
Jason Rouse <jrrouse88@gmail.com>
2017-09-25 11:59:45 -0700

    sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    
   end
 
   def edit
@@ -18,5 +20,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
