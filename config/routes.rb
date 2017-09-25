9806fd8213ac2836e6d558557053efeff5f4d4af
Jason Rouse <jrrouse88@gmail.com>
2017-09-25 14:19:14 -0700

    sessions and comic controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,32 @@
 Rails.application.routes.draw do
   root 'users#index'
-  
   resources :users
 
-#   Prefix|Verb   URI Pattern               Controller#Action
-#   root  |GET    /                         users#index
-#  users  |GET    /users(.:format)          users#index
-#         |POST   /users(.:format)          users#create
-#new_user |GET    /users/new(.:format)      users#new
-#edit_user|GET    /users/:id/edit(.:format) users#edit
-#   user  |GET    /users/:id(.:format)      users#show
-#        PATCH  /users/:id(.:format)        users#update
-#        PUT    /users/:id(.:format)        users#update
-#        DELETE /users/:id(.:format)        users#destroy
+  resources :sessions, only: [:new, :create]
+  delete 'logout' => 'sessions#destroy', as: :logout
 
+  resources :comics, except: [:index]
 end
+
+=begin
+     Prefix     Verb   URI Pattern                Controller#Action
+     root        GET    /                          users#index
+     users       GET    /users(.:format)           users#index
+                 POST   /users(.:format)           users#create
+     new_user    GET    /users/new(.:format)       users#new
+     edit_user   GET    /users/:id/edit(.:format)  users#edit
+     user        GET    /users/:id(.:format)       users#show
+                 PATCH  /users/:id(.:format)       users#update
+                 PUT    /users/:id(.:format)       users#update
+                 DELETE /users/:id(.:format)       users#destroy
+     sessions    POST   /sessions(.:format)        sessions#create
+     new_session GET    /sessions/new(.:format)    sessions#new
+     logout      DELETE /logout(.:format)          sessions#destroy
+     comics      POST   /comics(.:format)          comics#create
+     new_comic   GET    /comics/new(.:format)      comics#new
+     edit_comic  GET    /comics/:id/edit(.:format) comics#edit
+     comic       GET    /comics/:id(.:format)      comics#show
+                 PATCH  /comics/:id(.:format)      comics#update
+                 PUT    /comics/:id(.:format)      comics#update
+                 DELETE /comics/:id(.:format)      comics#destroy
+=end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925173623) do
+ActiveRecord::Schema.define(version: 20170925205308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170925173623) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "issue_number"
     t.index ["user_id"], name: "index_comics_on_user_id"
   end
 
